c98fd15d706921b0ab34d1cdc3dedc95d1728663
Kevin Zhu <450330386@qq.com>
2022-07-25 17:06:38 -0700

    Updates

---
+++ precache-manifest.13acd3084e8e380e262377be252ed063.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa3e126ba20b538dc7e25d8a6eaad649",
+    "url": "/Magpie-LuckyDraw/index.html"
+  },
+  {
+    "revision": "0569b44a5837a7cb6ec4",
+    "url": "/Magpie-LuckyDraw/static/css/main.f00ea47f.chunk.css"
+  },
+  {
+    "revision": "c9743761189b76fe75da",
+    "url": "/Magpie-LuckyDraw/static/js/2.acba6e45.chunk.js"
+  },
+  {
+    "revision": "fb828f47502fd5f5ab4102db26a99db6",
+    "url": "/Magpie-LuckyDraw/static/js/2.acba6e45.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0569b44a5837a7cb6ec4",
+    "url": "/Magpie-LuckyDraw/static/js/main.bdeccef3.chunk.js"
+  },
+  {
+    "revision": "add4b3c7f1a767079389",
+    "url": "/Magpie-LuckyDraw/static/js/runtime-main.3df11e18.js"
+  },
+  {
+    "revision": "aab060a4b44d6b68e229dd5c63b0d016",
+    "url": "/Magpie-LuckyDraw/static/media/albird.aab060a4.svg"
+  },
+  {
+    "revision": "622bb7a19e3c21858bded50c80ed5f50",
+    "url": "/Magpie-LuckyDraw/static/media/bg.622bb7a1.jpeg"
+  }
+]);
